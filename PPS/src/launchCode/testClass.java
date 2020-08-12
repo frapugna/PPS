f932d891e563d140ad17b642cb5c5de159aebd19
tommaso-senatori <senatom@virgilio.it>
2020-08-12 10:51:43 +0200

    added RNG in testClass

--- PPS/src/launchCode/testClass.java
+++ PPS/src/launchCode/testClass.java
@@ -1,8 +1,15 @@
 package launchCode;
 
+import java.util.Random;
+
 public class testClass {
 	
 	public testClass() {
 		System.out.println("Classe di tommy generata");
+	}
+	
+	public int RNG() {
+		Random r = new Random();
+		return r.nextInt();
 	}
 }
