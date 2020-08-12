5cad96afd8e7da92e9295f10b3e692e86303998f
jetixl <frapugna@gmail.com>
2020-08-12 11:02:37 +0200

    Added random D and G files just to pull the empty packages

--- PPS/src/launchCode/Main.java
+++ PPS/src/launchCode/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		testClass tmp = new testClass();
-
-		System.out.println(tmp.RNG());
-		new IAmAClass();
+	
 	}
 	
 }
