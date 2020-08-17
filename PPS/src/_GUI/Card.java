35e4c3c9b5f3a65b6bdb0876100d340a8d1b9dd1
jetixl <frapugna@gmail.com>
2020-08-17 17:28:27 +0200

    CharacterCard added

--- PPS/src/_GUI/Card.java
+++ PPS/src/_GUI/Card.java
@@ -2,6 +2,7 @@ package _GUI;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -10,15 +11,17 @@ import javax.swing.JPanel;
 import domainClasses.AbstractCardData;
 import domainClasses.Dealer;
 
-public class Card extends JPanel{
+public abstract class Card extends JPanel{
 	
 	static final int WIDTH = 150;
 	static final int HEIGHT = 200;
 	
+	
 	int xCoor;
 	int yCoor;
+	
 	ImageIcon image;
-	AbstractCardData startingData;
+	AbstractCardData cardType;
 	
 	public Card(int xCoor, int yCoor) {
 		super();
@@ -27,17 +30,16 @@ public class Card extends JPanel{
 		this.yCoor = yCoor;
 		
 		Dealer d = new Dealer();
-		startingData = d.drawCard();
+		cardType = d.drawCard();
 		
 		this.setSize(WIDTH, HEIGHT);
-		this.setBackground(Color.ORANGE);
+		//this.setBackground(Color.ORANGE);
 		
 		image = new ImageIcon("dog.jpg");
 		JLabel l = new JLabel(image);
 		
 		add(l);
-		
+	
 		l.setBounds(5, 5, 140, 140);
 	}
-	
 }
---
+++ PPS/src/_GUI/CharacterCard.java
@@ -0,0 +1,59 @@
+package _GUI;
+
+import java.awt.Rectangle;
+
+import javax.swing.JLabel;
+
+import domainClasses.MinionData;
+import domainClasses.PotionData;
+import domainClasses.RepairPotionData;
+import domainClasses.TrapData;
+import domainClasses.WeaponData;
+
+public class CharacterCard extends Card{
+	
+	final Rectangle STATUS_RECT = new Rectangle(5, 160, 70, 30);
+	int leftValue;
+	JLabel status;
+	
+	public CharacterCard(int xCoor, int yCoor) {
+		super(xCoor, yCoor);
+		if(cardType instanceof WeaponData) {
+			WeaponData wd = (WeaponData) cardType;
+			leftValue = wd.getDurability();
+			status = new JLabel(String.format("Uses: "+leftValue));
+			this.add(status);
+			status.setBounds(STATUS_RECT);
+			
+		}
+		else if(cardType instanceof TrapData) {
+			TrapData td = (TrapData) cardType;
+			leftValue = td.getDamage();
+			status = new JLabel(String.format("Power: "+leftValue));
+			this.add(status);
+			status.setBounds(STATUS_RECT);
+		}
+		else if(cardType instanceof RepairPotionData) {
+			RepairPotionData rpd = (RepairPotionData) cardType;
+			leftValue = rpd.getAmount();
+			status = new JLabel(String.format("Amount: "+leftValue));
+			this.add(status);
+			status.setBounds(STATUS_RECT);
+		}
+		else if(cardType instanceof PotionData) {
+			PotionData pd = (PotionData) cardType;
+			leftValue = pd.getHpHealed();
+			status = new JLabel(String.format("Healed: "+leftValue));
+			this.add(status);
+			status.setBounds(STATUS_RECT);
+		}
+		else{
+			MinionData md = (MinionData) cardType;
+			leftValue = md.gethp();
+			status = new JLabel(String.format("HP: "+leftValue));
+			this.add(status);
+			status.setBounds(STATUS_RECT);
+		}
+		
+	}
+}
