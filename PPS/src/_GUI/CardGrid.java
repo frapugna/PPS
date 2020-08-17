ef8c0603a98494ce8b995fa914b80dea52b32f36
tommaso-senatori <senatom@virgilio.it>
2020-08-17 17:55:47 +0200

    Added a method to initialize gridCard

--- PPS/src/_GUI/CardGrid.java
+++ PPS/src/_GUI/CardGrid.java
@@ -1,16 +1,73 @@
 package _GUI;
 
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
-public class CardGrid extends JPanel{
+public class CardGrid extends JPanel implements MouseListener{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	CharacterCard[][] cardMatrix;
+	
 	public CardGrid() {
 		super();
+		this.setLayout(null);
+		
+		initCardMatrix();
+	}
+	
+	private void initCardMatrix() {
+		
+		cardMatrix = new CharacterCard[3][3];
+		
+		for(int y = 0; y < 3; ++y) {
+			for(int x = 0; x < 3; ++x) {
+				
+				cardMatrix[y][x] = new CharacterCard(x, y);
+				cardMatrix[y][x].setLocation(x * CharacterCard.WIDTH, y * CharacterCard.HEIGHT);
+				cardMatrix[y][x].setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+				this.add(cardMatrix[y][x]);
+				
+			}
+		}
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 		
 	
--- PPS/src/_GUI/MainPanel.java
+++ PPS/src/_GUI/MainPanel.java
@@ -1,5 +1,6 @@
 package _GUI;
 
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
